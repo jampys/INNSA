16e74a2c4b6de26744959be757c44cb102420a7f
jampys <dario@innertrip.com.ar>
2014-10-14 20:31:36 -0300

    2 commit. Se agrega index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once("lib/config.php");
+require_once("lib/Conection.php");
+
+if(isset($_SESSION["ses_id"])){
+
+    if(!empty($_GET["accion"])){
+        $accion=$_GET["accion"];
+    }else
+    {
+        $accion="index";
+    }
+
+    if(is_file("controller/".$accion."Controller.php")){
+        require_once("controller/".$accion."Controller.php");
+    }else
+    {
+        require_once("controller/errorController.php");
+    }
+
+
+}else
+{
+    //modificado Dario
+    if($_GET["accion"]=="error"){
+        require_once("controller/errorController.php");
+    }
+    else{
+        require_once("controller/LoginController.php");
+    }
+
+}
+
+
+?>
