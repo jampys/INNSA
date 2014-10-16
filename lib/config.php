4e744ef3c0696526b8d78f990b8ac0a20dca3f9c
jampys <dario@innertrip.com.ar>
2014-10-16 20:47:31 -0300

    Se agregaron en el config.php las clases para crear y manipular conexiones oracle. Se agrego tambien una clase prueba.php para probar la conexion.

--- lib/config.php
+++ lib/config.php
@@ -6,7 +6,7 @@ class Conexion  {// se declara una clase para hacer la conexion con la base de d
 
     var $con;
 
-    function Conexion() {
+    function __construct(){
         // se definen los datos del servidor de base de datos
         $conection['server']="localhost";  //host
         $conection['user']="root";         //  usuario
@@ -20,7 +20,9 @@ class Conexion  {// se declara una clase para hacer la conexion con la base de d
             mysql_select_db($conection['base']);
             $this->con=$conect;
         }
+
     }
+
 
     function getConexion(){ // devuelve la conexion
         return $this->con;
@@ -39,11 +41,11 @@ class Conexion  {// se declara una clase para hacer la conexion con la base de d
 
 class sQuery   // se declara una clase para poder ejecutar las consultas, esta clase llama a la clase anterior
 {
-
     var $coneccion;
     var $consulta;
-    var $resultados;
-    function sQuery()  // constructor, solo crea una conexion usando la clase "Conexion"
+    var $resultados;  //DARIO: no lo usa nunca
+
+    function __construct()  // constructor, solo crea una conexion usando la clase "Conexion"
     {
         $this->coneccion= new Conexion();
     }
@@ -52,6 +54,7 @@ class sQuery   // se declara una clase para poder ejecutar las consultas, esta c
         $this->consulta= mysql_query($cons,$this->coneccion->getConexion());
         return $this->consulta;
     }
+
     function getResults()   // retorna la consulta en forma de result.
     {return $this->consulta;}
 
@@ -67,8 +70,8 @@ class sQuery   // se declara una clase para poder ejecutar las consultas, esta c
     function getAffect() // devuelve las cantidad de filas afectadas     //DARIO: este metodo es igual al de arriba
     {return mysql_affected_rows($this->coneccion->getConexion()) ;}
 
-    function fetchAll()
-    {
+
+    function fetchAll(){
         $rows=array();
         if ($this->consulta)
         {
@@ -81,8 +84,98 @@ class sQuery   // se declara una clase para poder ejecutar las consultas, esta c
     }
 }
 
+//****************************************************************************************************************
+
+class ConexionOracle extends Conexion{
+
+    //var $con se hereda
+
+    function __construct() {
+
+        $conection['server']="localhost";  //host
+        $conection['user']="dario";         //  usuario
+        $conection['pass']="dario";             //password
+        $conection['base']="INNSADB";           //base de datos
+
+        $conect = oci_connect($conection['user'], $conection['pass'], $conection['server']);
+        //conexion oracle
+        if (!$conect) {
+            $m = oci_error();
+            echo $m['message'], "\n";
+            exit;
+        } else {
+            echo "Conexión con éxito a Oracle!";
+            $this->con=$conect;
+        }
+
+    }
+
+   //function getConexion() se hereda y no se modifica
+
+    function Close(){  // se redefine
+        oci_close($this->$con);
+    }
+
+    //public static function ruta() se hereda y no se modifica
+
+}
+
+//**************************************************************************************
+
+class sQueryOracle extends sQuery   // se declara una clase para poder ejecutar las consultas, esta clase llama a la clase anterior
+{
+    //var $coneccion; //se hereda
+    //var $consulta;  //se hereda
+    //var $resultados;  //se hereda
+
+    function __construct()  { //se sobreescribe ...funciona ok
+        //$c= new ConexionOracle();
+        $this->coneccion=new ConexionOracle();
+        echo 'se ejecuto el constructor de la clase sQueryOracle';
+    }
 
 
+    function executeQuery($cons)  // se sobreescribe........funciona ok
+    {
+        $this->consulta=oci_parse($this->coneccion->getConexion(), $cons);
+        oci_execute($this->consulta);
+        return $this->consulta;
+    }
 
+    //getResults() no se sobreescribe
+
+   //Close()    no se sobrescribe
+
+    function Clean() // libera la consulta
+    {oci_free_statement($this->consulta);}
+
+
+    function getResultados() // debuelve la cantidad de registros encontrados
+    {
+        //return oci_num_rows($this->coneccion->getConexion()) ;}
+        return oci_num_rows($this->consulta);}
+
+    function getAffect() // devuelve las cantidad de filas afectadas     //DARIO: este metodo es igual al de arriba
+    {return oci_num_rows($this->consulta);}
+
+
+    //esta devuelve todos los registros en un array..... se debe sobreescribir
+    //esta funcion devuelve un array de 2 dimensiones con los resultados de la consulta
+    function fetchAll()
+    {
+        $rows=array();
+        if ($this->consulta)
+        {
+            //la funcion oci_fetch_array devuelve cada fila de la consulta en forma de array
+            while($row = oci_fetch_array($this->consulta, OCI_ASSOC))
+            {   //luego cada fila (como un array) se agrega a otro array... creando un array de 2 dimsnesiones
+                $rows[]=$row;
+            }
+        }
+        return $rows;
+    }
+
+
+}
 
 ?>
---
+++ lib/prueba.php
@@ -0,0 +1,38 @@
+<?php
+require_once('config.php');
+
+$s=new sQueryOracle();
+$query="select * from cursos";
+$s->executeQuery($query);
+$r=$s->fetchAll();
+
+echo"<hr/>";
+echo 'Cantidad de filas afectadas:'.$s->getAffect();
+echo '<br/>';
+echo 'Liberacion de la consulta:'.$s->clean();
+//print_r($r);
+
+?>
+
+
+
+<table border="1" width="60%" cellspacing="0">
+    <tr>
+        <td>ID</td>
+        <td>NOMBRE</td>
+        <td>DESCRIPCION</td>
+    </tr>
+
+        <?php
+        foreach ($r as $fila){
+            ?>
+            <tr>
+                <td><?php echo $fila['ID_CURSO']; ?></td>
+                <td><?php echo $fila['NOMBRE']; ?></td>
+                <td><?php echo $fila['DESCRIPCION']; ?></td>
+
+            </tr>
+        <?php
+        }
+        ?>
+</table>
